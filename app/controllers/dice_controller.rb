cee6248a390df1e0bcf5d80ac67c0f5f34142df5
Bella Rose Wilson <bwilson9893@gmail.com>
2025-09-17 18:25:06 +0000

    fixed controller w params

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -47,17 +47,12 @@ class DiceController < ApplicationController
     render({ :template => "dice_templates/result_five_four" })
   end
 
- def roll_flexible
+  def roll_dynamic
     @num_dice = params.fetch("number_of_dice").to_i
-    @sides = params.fetch("how_many_sides").to_i
-
+    @sides = params.fetch("sides").to_i
     @rolls = []
-
     @num_dice.times do
-      dice = rand(1..@sides)
-      @rolls.push(dice)
+      die = rand(1..@sides)
+      @rolls.push(die)
     end
-
-    render({ :template => "dice_templates/result_flexible" })
-  end
 end
